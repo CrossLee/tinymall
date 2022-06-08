01fcaf55bd59ebae0d04bd15cddb8a8eaded3417
cross <mag_lee@126.com>
2022-06-08 22:40:11 +0800

    init

---
+++ tiny-goods/src/main/java/com/qdstorm/tinymall/tinygoods/TinyGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.qdstorm.tinymall.tinygoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyGoodsApplication.class, args);
+    }
+
+}
---
+++ tiny-goods/src/test/java/com/qdstorm/tinymall/tinygoods/TinyGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qdstorm.tinymall.tinygoods;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TinyGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
